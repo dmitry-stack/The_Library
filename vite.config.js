ade9766e63c5228e3d14e744ff07a6981e73e1e6
Dmitry Hais <hadminsk@icloud.com>
2026-06-28 01:53:16 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({
   publicDir: "public",
+  base: "/The_Library/",
 
   plugins: [cssInjectedByJsPlugin()],
 
